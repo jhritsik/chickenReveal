655031b432e40f65587fc416339ea3816a76ca2d
jhritsik <jhritsik@gmail.com>
2017-11-02 11:12:48 -0400

    more base js

--- js/project.js
+++ js/project.js
@@ -1,9 +1,9 @@
 $(function() {
     var container = $("#container");
-    var count = 70;
+    var count = 33;
     var square = 'square';
     for (var i=0; i<count; i++) {
-        container.append('<div class="' + square + '">' + i + '</div>');
+        container.append('<div class="' + square + ' ' + i + '">' + i + '</div>');
        	//$('.square').css({"height":"100px", "width":"100", "border":"1px solid black", "float":"left"});
     }
   $('.square').click(function(){
